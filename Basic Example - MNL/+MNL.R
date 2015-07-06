3edb5fa4f7bb3ca8429486e1a7e10f500291f7d5
unknown <jeff.keller@10162W.i-rsg.com>
2015-07-06 12:18:22 -0400

    Updated MNL example

--- Basic Example - MNL/+MNL.R
+++ Basic Example - MNL/+MNL.R
@@ -1,7 +1,7 @@
 # ------------------
 # Code for a Multinomial Logit Model with Random Coefficients
 # 
-# Jeff Dumont
+# Jeff Dumont & Jeff Keller
 #
 # ------------------
 
@@ -15,22 +15,22 @@ library(RSGHB)
 
 # assumes that respondents are identified with a ID column
 # also assumes that the data is sorted by respondent then experiment
-choicedata <- read.table("Data_simulated.dat", sep = "\t", header = TRUE)
+choicedata <- read.table("mnl_simulated.dat", sep = "\t", header = TRUE)
 
 # Specify any variables here that you'd like to use in the
 # utility equations in the likelihood function below
 # These can be any variables within the data or transformations of
 # those variables
-TT1     <- choicedata$tt1
-TT2     <- choicedata$tt2
-TOLL2   <- choicedata$toll2
+TT1   <- choicedata$tt1
+TT2   <- choicedata$tt2
+TOLL2 <- choicedata$toll2
 
 # The choice vectors
 # Dummying coding the choice vector allows for easier coding of the 
 # the likelihood calculations. So we will have one column for each 
 # alternative in the design
-choice1    <- (choicedata$Choice == 1)
-choice2    <- (choicedata$Choice == 2)
+choice1 <- (choicedata$Choice == 1)
+choice2 <- (choicedata$Choice == 2)
 
 # ------------------
 # ESTIMATION CONTROL
@@ -38,7 +38,7 @@ choice2    <- (choicedata$Choice == 2)
 # ?doHB for more estimation options
 # ------------------
 
-modelname <- "MNL"            # used for output
+modelname <- "MNL" # used for output
 
 # Names for the Random Variables
 gVarNamesNormal <- c("ASC1", "BTime", "BCost")
@@ -55,14 +55,14 @@ gVarNamesNormal <- c("ASC1", "BTime", "BCost")
 gDIST <- c(1, 1, 1)
 
 # STARTING VALUES
-svN <- c(0, 0, 0)               # for the random coefficients
+svN <- c(0, 0, 0)   # for the random coefficients
 # The selection of the mean here is important when working with non-normal distributions
 
 # ITERATION SETTINGS
-gNCREP    <- 30000            # Number of iterations to use prior to convergence
-gNEREP    <- 20000 	          # Number of iterations to keep for averaging after convergence has been reached
-gNSKIP    <- 1			     # Number of iterations to do in between retaining draws for averaging
-gINFOSKIP <- 250              # How frequently to print info about the iteration process
+gNCREP    <- 5000   # Number of iterations to use prior to convergence
+gNEREP    <- 5000 	# Number of iterations to keep for averaging after convergence has been reached
+gNSKIP    <- 1			# Number of iterations to do in between retaining draws for averaging
+gINFOSKIP <- 100    # How frequently to print info about the iteration process
 
 # CONTROL LIST TO PASS TO doHB
 control <- list(
@@ -74,10 +74,7 @@ control <- list(
      gNEREP = gNEREP,
      gNSKIP = gNSKIP,
      gINFOSKIP = gINFOSKIP,
-     write.results = TRUE,
-     gSeed = 1987,
-     nodiagnostics = TRUE, # Set this to FALSE to see initial model diagnostics
-     verbose = FALSE       # Set this to TRUE to see real-time progress printed and plotted
+     gSeed = 1987
 )
 
 # ------------------
@@ -85,25 +82,33 @@ control <- list(
 # USE:     Calculates the likelihood of choice | B
 #          Returns likelihood values for each observation
 # NOTES:   This is where the bulk of the computation resides so coding this efficiently
-#	      is essential to reducing run time.
+#	         is essential to reducing run time.
 # ------------------
-likelihood <- function(fc, b)
-{  
+likelihood <- function(fc, b) {
      
      # random coefficients
      cc     <- 1
      ASC1   <- b[, cc]; cc <- cc + 1
      Btime  <- b[, cc]; cc <- cc + 1
-     Btoll  <- b[, cc]; cc <- cc + 1  
+     Btoll  <- b[, cc]; cc <- cc + 1
      
-     v1 <- ASC1       + Btime * TT1                   
-     v2 <-              Btime * TT2 + Btoll * TOLL2   
+     v1 <- ASC1 + Btime * TT1
+     v2 <-        Btime * TT2 + Btoll * TOLL2
      
-     p  <- (exp(v1)*choice1 + exp(v2)*choice2) / (exp(v1) + exp(v2))
+     p  <- (exp(v1) * choice1 + exp(v2) * choice2) / (exp(v1) + exp(v2))
      
      return(p)
 }
 
 # Estimate the model
 model <- doHB(likelihood, choicedata, control)
+
+# Plot model statistics
+plot(model)
+plot(model, type = "A")
+
+# Save model object
 save(model, file = paste0(model$modelname, ".RData"))
+
+# Save in CSV format (Sawtooth-esque)
+writeModel(model)
