24cfe7c7d2f7fc0eb899be4421facf0e531c2805
unknown <jeff.keller@10162W.i-rsg.com>
2015-07-14 09:54:24 -0400

    Updated nested logit example.

--- 7 Nested Logit/+NL.R
+++ 7 Nested Logit/+NL.R
@@ -1,11 +1,11 @@
-# ------------------
+# ---------------------------------------------------------------------
 # Code for a Nested Logit Model
 # This assumes that alt 1 and 2 are nested together and alternatives
 # 3 and 4 are nested together
 # 
-# Jeff Dumont
+# Jeff Dumont & Jeff Keller
 #
-# ------------------
+# ---------------------------------------------------------------------
 
 library(RSGHB)
 
@@ -16,7 +16,7 @@ library(RSGHB)
 # ------------------
 # assumes that respondents are identified with a ID column
 # also assumes that the data is sorted by respondent then experiment
-choicedata <- read.table("Data_simulated.csv", sep = ",", header = TRUE)
+choicedata <- read.table("nl_simulated.csv", sep = ",", header = TRUE)
 
 # Specify any variables here that you'd like to use in the
 # utility equations in the likelihood function below
@@ -35,17 +35,17 @@ y4 <- choicedata$y4
 # Dummying coding the choice vector allows for easier coding of the 
 # the likelihood calculations. So we will have one column for each 
 # alternative in the design
-choice1    <- (choicedata$choice == 1)
-choice2    <- (choicedata$choice == 2)
-choice3    <- (choicedata$choice == 3)
-choice4    <- (choicedata$choice == 4)
+choice1 <- (choicedata$choice == 1)
+choice2 <- (choicedata$choice == 2)
+choice3 <- (choicedata$choice == 3)
+choice4 <- (choicedata$choice == 4)
 
-# ------------------
+# ----------------------------------------
 # ESTIMATION CONTROL
 # Setting control list for estimation
 # ?doHB for more estimation options
-# ------------------
-modelname <- "Nested_Logit"     	# used for output
+# ----------------------------------------
+modelname <- "Nested_Logit" # used for output
 
 # Names for the normal variables
 gVarNamesNormal <- c("Bx", "By")
@@ -65,14 +65,14 @@ gDIST <- c(1, 1)
 gVarNamesFixed <- c("lambda1", "lambda2")
 
 # STARTING VALUES
-svN <- c(0, 0)                 # for the random coefficients
-FC  <- c(0.5, 0.5)             # for the fixed coefficients
+svN <- c(0, 0)     # for the random coefficients
+FC  <- c(0.5, 0.5) # for the fixed coefficients
 
 # ITERATION SETTINGS
-gNCREP    <- 10000  	  # Number of iterations to use prior to convergence
-gNEREP    <- 10000 	       # Number of iterations to keep for averaging after convergence has been reached
-gNSKIP    <- 1			  # Number of iterations to do in between retaining draws for averaging
-gINFOSKIP <- 250           # How frequently to print info about the iteration process
+gNCREP    <- 5000  # Number of iterations to use prior to convergence
+gNEREP    <- 5000  # Number of iterations to keep for averaging after convergence has been reached
+gNSKIP    <- 1		 # Number of iterations to do in between retaining draws for averaging
+gINFOSKIP <- 100   # How frequently to print info about the iteration process
 
 control <- list(
      modelname = modelname,
@@ -85,21 +85,17 @@ control <- list(
      gNEREP = gNEREP,
      gNSKIP = gNSKIP,
      gINFOSKIP = gINFOSKIP,
-     write.results = TRUE,
-     gSeed = 1987,
-     nodiagnostics = TRUE, # Set this to FALSE to see initial model diagnostics
-     verbose = FALSE       # Set this to TRUE to see real-time progress printed and plotted
+     gSeed = 1987
 )
 
-# ------------------
+# --------------------------------------------------------------------------------------
 # likelihood
 # USE:     Calculates the likelihood of choice | B
 #          Returns likelihood values for each observation
 # NOTES:   This is where the bulk of the computation resides so coding this efficiently
 #          is essential to reducing run time.
-# ------------------
-likelihood <- function(fc, b)
-{
+# --------------------------------------------------------------------------------------
+likelihood <- function(fc, b) {
 
   cc <- 1
   Bx <- b[, cc]; cc <- cc + 1
@@ -129,6 +125,17 @@ likelihood <- function(fc, b)
   return(p)
 }
 
+
 # Estimate the model
 model <- doHB(likelihood, choicedata, control)
+
+# Plot model statistics
+plot(model)
+plot(model, type = "A")
+plot(model, type = "F")
+
+# Save model object
 save(model, file = paste0(model$modelname, ".RData"))
+
+# Save in CSV format (Sawtooth-esque)
+writeModel(model)
