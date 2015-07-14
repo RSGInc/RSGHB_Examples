1ace2cbefe26cecbcc71abe5d46148bd84f82a3b
unknown <jeff.keller@10162W.i-rsg.com>
2015-07-14 10:06:40 -0400

    Updated ECL example

--- 8 Error Components Logit/+ECL.R
+++ 8 Error Components Logit/+ECL.R
@@ -1,9 +1,9 @@
-# ------------------
+# -----------------------------------
 # Code for a Error Components Logit
 # 
-# Jeff Dumont
+# Jeff Dumont & Jeff Keller
 #
-# ------------------
+# -----------------------------------
 
 library(RSGHB)
 
@@ -12,7 +12,7 @@ library(RSGHB)
 # ------------------
 # assumes that respondents are identified with a ID column
 # also assumes that the data is sorted by respondent then experiment
-choicedata <- read.table("Data_simulated.csv", sep = ",", header = TRUE)
+choicedata <- read.table("ecl_simulated.csv", sep = ",", header = TRUE)
 
 # Specify any variables here that you'd like to use in the
 # utility equations in the likelihood function below
@@ -29,17 +29,17 @@ y3 <- choicedata$y3
 # Dummying coding the choice vector allows for easier coding of the 
 # the likelihood calculations. So we will have one column for each 
 # alternative in the design
-choice1    <- (choicedata$choice == 1)
-choice2    <- (choicedata$choice == 2)
-choice3    <- (choicedata$choice == 3)
+choice1 <- (choicedata$choice == 1)
+choice2 <- (choicedata$choice == 2)
+choice3 <- (choicedata$choice == 3)
 
-# ------------------
+# ----------------------------------------
 # ESTIMATION CONTROL
 # Setting control list for estimation
 # ?doHB for more estimation options
-# ------------------
+# ----------------------------------------
 
-modelname <- "Error Components"		# used for output
+modelname <- "Error Components"	# used for output
 
 # Names for the random variables
 gVarNamesNormal <- c("Bx", "By", "EC")
@@ -57,20 +57,19 @@ gVarNamesNormal <- c("Bx", "By", "EC")
 gDIST <- c(1, 1, 1)
 
 # STARTING VALUES
-FC <- c()                  # for the fixed coefficients
-
-svN <- c(0, 0, 0)          # for the random coefficients
-                           # The selection of the mean here is important when working with non-normal distributions
+FC <- c()           # for the fixed coefficients
+svN <- c(0, 0, 0)   # for the random coefficients
+                    # The selection of the mean here is important when working with non-normal distributions
 
 # Here you can specify random parameters to have fixed means
 # Note that a NA represents unconstrained estimation of the mean value
 fixedA <- c(NA, NA, 0)
 
 # ITERATION SETTINGS
-gNCREP    <- 10000  	  # Number of iterations to use prior to convergence
-gNEREP    <- 10000 	       # Number of iterations to keep for averaging after convergence has been reached
-gNSKIP    <- 1			  # Number of iterations to do in between retaining draws for averaging
-gINFOSKIP <- 250           # How frequently to print info about the iteration process
+gNCREP    <- 5000  	# Number of iterations to use prior to convergence
+gNEREP    <- 5000 	# Number of iterations to keep for averaging after convergence has been reached
+gNSKIP    <- 1			# Number of iterations to do in between retaining draws for averaging
+gINFOSKIP <- 100    # How frequently to print info about the iteration process
 
 # CONTROL LIST TO PASS TO doHB
 control <- list(
@@ -83,22 +82,18 @@ control <- list(
      gNSKIP = gNSKIP,
      gINFOSKIP = gINFOSKIP,
      fixedA = fixedA,
-     write.results = TRUE,
-     gSeed = 1987,
-     nodiagnostics = TRUE, # Set this to FALSE to see initial model diagnostics
-     verbose = FALSE       # Set this to TRUE to see real-time progress printed and plotted
+     gSeed = 1987
 )
 
 
-# ------------------
+# ----------------------------------------------------------------------------------------
 # likelihood
 # USE:     Calculates the likelihood of choice | B
 #          Returns likelihood values for each observation
 # NOTES:   This is where the bulk of the computation resides so coding this efficiently
 #          is essential to reducing run time.
-# ------------------
-likelihood <- function(fc, b)
-{
+# ----------------------------------------------------------------------------------------
+likelihood <- function(fc, b) {
 
   cc <- 1
   Bx  <- b[, cc]; cc <- cc + 1
@@ -116,4 +111,13 @@ likelihood <- function(fc, b)
 
 # Estimate the model
 model <- doHB(likelihood, choicedata, control)
+
+# Plot model statistics
+plot(model)
+plot(model, type = "A")
+
+# Save model object
 save(model, file = paste0(model$modelname, ".RData"))
+
+# Save in CSV format (Sawtooth-esque)
+writeModel(model)
